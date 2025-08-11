d3686222f783ee02cde44f3d2467bc317dd30b5a
AlienPoodle <59735305+alienpoodle@users.noreply.github.com>
2025-08-10 21:37:26 -0400

    add firebase

---
+++ js/firebaseConfig.js
@@ -0,0 +1,9 @@
+// Place your Firebase config here. This file should not be committed to version control.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
